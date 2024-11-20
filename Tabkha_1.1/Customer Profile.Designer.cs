40ba788fbe24dbe0582c189b60df29460583dac2
Hagar Ghazal <hagarramadan402@gmail.com>
2024-11-20 23:33:53 +0200

    add customer Profile

--- Tabkha_1.1/Customer Profile.Designer.cs
+++ Tabkha_1.1/Customer Profile.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Customer_Profile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1147, 640);
+            this.Name = "Customer_Profile";
+            this.ShowInTaskbar = false;
             this.Text = "Customer_Profile";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
