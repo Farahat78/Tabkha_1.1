115439c5b7a29e52ab7760574d489e6a81428cd3
Hagar Ghazal <hagarramadan402@gmail.com>
2024-11-22 00:43:47 +0200

    add register page

---
+++ Tabkha_1.1/Owner Register.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Tabkha_1._1
+{
+    partial class Owner_Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Owner_Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 696);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Owner_Register";
+            this.Text = "Owner_Register";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
