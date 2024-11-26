7b087c447f2e9b4689326e2a5c9f490e18a6a4da
Hossam Mostafa <shicoo9911@gmail.com>
2024-11-26 22:37:53 +0200

    done

--- Tabkha_1.1/Customer Profile.cs
+++ Tabkha_1.1/Customer Profile.cs
@@ -15,11 +15,6 @@ namespace Tabkha_1._1
         public Customer_Profile()
         {
             InitializeComponent();
-            pnl_show.Hide();
-            //btn_editEmail.Hide();
-            //btn_editBdate.Hide();
-            //btn_editPnumber.Hide();
-            //btn_editName.Hide();
         }
         public void LoadFormIntoPanel(Form Form)
         {
@@ -32,28 +27,15 @@ namespace Tabkha_1._1
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            pnl_hide.Hide();
             Gpnl_choices.Hide();
             pnl_show.Show();
         }
 
         private void img_show_Click(object sender, EventArgs e)
         {
-            pnl_hide.Show();
             Gpnl_choices.Show();
             pnl_show.Hide();
         }
-
-        private void rbtn_profile_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void rbtn_manageProfile_Click(object sender, EventArgs e)
-        {
-            
-        }
-
 
         private void img_close_Click(object sender, EventArgs e)
         {
@@ -70,42 +52,6 @@ namespace Tabkha_1._1
             login login = new login();
             login.Show();
             this.Hide();
-        }
-
-        private void textBox5_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void rbtn_orderData_CheckedChanged(object sender, EventArgs e)
-        {
-         
-            
-        }
-
-        private void pnl_order_Click(object sender, EventArgs e)
-        {
-           
-        }
-
-        private void rbtn_orderData_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void btn_preiousOrder_Click(object sender, EventArgs e)
-        {
-            
         }
 
         private void btn_viewProfile_Click(object sender, EventArgs e)
