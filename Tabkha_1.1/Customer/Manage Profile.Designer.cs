61e86e9e8f3c93072adfdb9a10339a6ba5afc2eb
Hossam Mostafa <shicoo9911@gmail.com>
2024-11-28 20:27:14 +0200

    done

--- Tabkha_1.1/Customer/Manage Profile.Designer.cs
+++ Tabkha_1.1/Customer/Manage Profile.Designer.cs
@@ -28,243 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.pnl_edit = new System.Windows.Forms.Panel();
-            this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
-            this.img_customer_profile = new System.Windows.Forms.PictureBox();
             this.btn_edit_img = new System.Windows.Forms.Button();
-            this.btn_editEmail = new System.Windows.Forms.Button();
-            this.btn_editBdate = new System.Windows.Forms.Button();
-            this.btn_editPnumber = new System.Windows.Forms.Button();
-            this.btn_editName = new System.Windows.Forms.Button();
-            this.txtbox_name = new System.Windows.Forms.TextBox();
-            this.txtbox_Bdate = new System.Windows.Forms.TextBox();
-            this.txtbox_email = new System.Windows.Forms.TextBox();
-            this.txtbox_Pnumber = new System.Windows.Forms.TextBox();
-            this.lbl_Pnumber = new System.Windows.Forms.Label();
-            this.lbl_Bdate = new System.Windows.Forms.Label();
-            this.lbl_email = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
             this.lbl_name = new System.Windows.Forms.Label();
-            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.guna2Elipse3 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.guna2Elipse4 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.guna2Elipse5 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.guna2Elipse6 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.pnl_edit.SuspendLayout();
-            this.guna2ShadowPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.img_customer_profile)).BeginInit();
+            this.lbl_email = new System.Windows.Forms.Label();
+            this.lbl_Pnumber = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.txtbox_email = new Tabkha_1._1.CustomTextBox();
+            this.txtbox_Pnumber = new Tabkha_1._1.CustomTextBox();
+            this.txtbox_password = new Tabkha_1._1.CustomTextBox();
+            this.txtbox_name = new Tabkha_1._1.CustomTextBox();
             this.SuspendLayout();
-            // 
-            // guna2Elipse1
-            // 
-            this.pnl_edit.BackColor = System.Drawing.Color.Transparent;
-            this.pnl_edit.Controls.Add(this.guna2ShadowPanel1);
-            this.pnl_edit.Controls.Add(this.btn_editEmail);
-            this.pnl_edit.Controls.Add(this.btn_editBdate);
-            this.pnl_edit.Controls.Add(this.btn_editPnumber);
-            this.pnl_edit.Controls.Add(this.btn_editName);
-            this.pnl_edit.Controls.Add(this.txtbox_name);
-            this.pnl_edit.Controls.Add(this.txtbox_Bdate);
-            this.pnl_edit.Controls.Add(this.txtbox_email);
-            this.pnl_edit.Controls.Add(this.txtbox_Pnumber);
-            this.pnl_edit.Controls.Add(this.lbl_Pnumber);
-            this.pnl_edit.Controls.Add(this.lbl_Bdate);
-            this.pnl_edit.Controls.Add(this.lbl_email);
-            this.pnl_edit.Controls.Add(this.lbl_name);
-            this.pnl_edit.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnl_edit.Location = new System.Drawing.Point(0, 0);
-            this.pnl_edit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pnl_edit.Name = "pnl_edit";
-            this.pnl_edit.Size = new System.Drawing.Size(800, 428);
-            this.pnl_edit.TabIndex = 21;
-            // 
-            // guna2Elipse2
-            // 
-            this.guna2Elipse2.BorderRadius = 15;
             // 
             // btn_edit_img
             // 
-            this.guna2Elipse3.BorderRadius = 15;
+            this.btn_edit_img.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(217)))), ((int)(((byte)(188)))));
+            this.btn_edit_img.Location = new System.Drawing.Point(13, 13);
+            this.btn_edit_img.Name = "btn_edit_img";
+            this.btn_edit_img.Size = new System.Drawing.Size(75, 23);
+            this.btn_edit_img.TabIndex = 0;
+            this.btn_edit_img.Text = "Upload";
+            this.btn_edit_img.UseVisualStyleBackColor = false;
             // 
-            // guna2Elipse4
+            // btn_save
             // 
-            this.guna2Elipse4.BorderRadius = 15;
+            this.btn_save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(217)))), ((int)(((byte)(188)))));
+            this.btn_save.Location = new System.Drawing.Point(379, 148);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 1;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
             // 
-            // guna2Elipse5
+            // btn_edit
             // 
-            this.guna2Elipse5.BorderRadius = 15;
-            // 
-            // guna2Elipse6
-            // 
-            this.guna2Elipse6.BorderRadius = 100;
-            // 
-            // guna2Elipse7
-            // 
-            this.guna2Elipse7.BorderRadius = 20;
-            this.guna2Elipse7.TargetControl = this.btn_edit;
-            // 
-            // guna2Elipse8
-            // 
-            this.guna2Elipse8.BorderRadius = 20;
-            this.guna2Elipse8.TargetControl = this.btn_save;
+            this.btn_edit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(217)))), ((int)(((byte)(188)))));
+            this.btn_edit.Location = new System.Drawing.Point(275, 148);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(75, 23);
+            this.btn_edit.TabIndex = 2;
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.UseVisualStyleBackColor = false;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
             // 
             // lbl_name
             // 
             this.lbl_name.AutoSize = true;
-            this.lbl_name.BackColor = System.Drawing.Color.White;
-            this.lbl_name.Location = new System.Drawing.Point(125, 25);
+            this.lbl_name.Location = new System.Drawing.Point(108, 13);
             this.lbl_name.Name = "lbl_name";
-            this.lbl_name.Size = new System.Drawing.Size(44, 16);
-            this.lbl_name.TabIndex = 32;
-            this.lbl_name.Text = "Name";
+            this.lbl_name.Size = new System.Drawing.Size(76, 16);
+            this.lbl_name.TabIndex = 7;
+            this.lbl_name.Text = "User Name";
             // 
             // lbl_email
             // 
             this.lbl_email.AutoSize = true;
-            this.lbl_email.BackColor = System.Drawing.Color.White;
-            this.lbl_email.Location = new System.Drawing.Point(125, 74);
+            this.lbl_email.Location = new System.Drawing.Point(108, 41);
             this.lbl_email.Name = "lbl_email";
             this.lbl_email.Size = new System.Drawing.Size(41, 16);
-            this.lbl_email.TabIndex = 33;
+            this.lbl_email.TabIndex = 8;
             this.lbl_email.Text = "Email";
-            // 
-            // lbl_Bdate
-            // 
-            this.lbl_Bdate.AutoSize = true;
-            this.lbl_Bdate.BackColor = System.Drawing.Color.White;
-            this.lbl_Bdate.Location = new System.Drawing.Point(125, 122);
-            this.lbl_Bdate.Name = "lbl_Bdate";
-            this.lbl_Bdate.Size = new System.Drawing.Size(79, 16);
-            this.lbl_Bdate.TabIndex = 34;
-            this.lbl_Bdate.Text = "Date of Birth";
             // 
             // lbl_Pnumber
             // 
             this.lbl_Pnumber.AutoSize = true;
-            this.lbl_Pnumber.BackColor = System.Drawing.Color.White;
-            this.lbl_Pnumber.Location = new System.Drawing.Point(125, 171);
+            this.lbl_Pnumber.Location = new System.Drawing.Point(108, 71);
             this.lbl_Pnumber.Name = "lbl_Pnumber";
             this.lbl_Pnumber.Size = new System.Drawing.Size(97, 16);
-            this.lbl_Pnumber.TabIndex = 35;
+            this.lbl_Pnumber.TabIndex = 9;
             this.lbl_Pnumber.Text = "Phone Number";
             // 
-            // txtbox_Pnumber
+            // lbl_password
             // 
-            this.txtbox_Pnumber.Location = new System.Drawing.Point(238, 168);
-            this.txtbox_Pnumber.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtbox_Pnumber.Name = "txtbox_Pnumber";
-            this.txtbox_Pnumber.Size = new System.Drawing.Size(352, 22);
-            this.txtbox_Pnumber.TabIndex = 36;
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(108, 107);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(67, 16);
+            this.lbl_password.TabIndex = 10;
+            this.lbl_password.Text = "Password";
             // 
             // txtbox_email
             // 
-            this.txtbox_email.Location = new System.Drawing.Point(238, 69);
-            this.txtbox_email.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtbox_email.Location = new System.Drawing.Point(222, 38);
             this.txtbox_email.Name = "txtbox_email";
-            this.txtbox_email.Size = new System.Drawing.Size(352, 22);
-            this.txtbox_email.TabIndex = 37;
+            this.txtbox_email.Size = new System.Drawing.Size(328, 22);
+            this.txtbox_email.TabIndex = 6;
+            this.txtbox_email.WatermarkColor = System.Drawing.Color.Gray;
+            this.txtbox_email.WatermarkText = "Enter email here";
             // 
-            // txtbox_Bdate
+            // txtbox_Pnumber
             // 
-            this.txtbox_Bdate.Location = new System.Drawing.Point(238, 117);
-            this.txtbox_Bdate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtbox_Bdate.Name = "txtbox_Bdate";
-            this.txtbox_Bdate.Size = new System.Drawing.Size(352, 22);
-            this.txtbox_Bdate.TabIndex = 38;
+            this.txtbox_Pnumber.Location = new System.Drawing.Point(222, 68);
+            this.txtbox_Pnumber.Name = "txtbox_Pnumber";
+            this.txtbox_Pnumber.Size = new System.Drawing.Size(328, 22);
+            this.txtbox_Pnumber.TabIndex = 5;
+            this.txtbox_Pnumber.WatermarkColor = System.Drawing.Color.Gray;
+            this.txtbox_Pnumber.WatermarkText = "Enter phone number here";
+            // 
+            // txtbox_password
+            // 
+            this.txtbox_password.Location = new System.Drawing.Point(222, 104);
+            this.txtbox_password.Name = "txtbox_password";
+            this.txtbox_password.Size = new System.Drawing.Size(328, 22);
+            this.txtbox_password.TabIndex = 4;
+            this.txtbox_password.WatermarkColor = System.Drawing.Color.Gray;
+            this.txtbox_password.WatermarkText = "Enter password here";
             // 
             // txtbox_name
             // 
-            this.txtbox_name.Location = new System.Drawing.Point(238, 20);
-            this.txtbox_name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtbox_name.Location = new System.Drawing.Point(222, 10);
             this.txtbox_name.Name = "txtbox_name";
-            this.txtbox_name.Size = new System.Drawing.Size(352, 22);
-            this.txtbox_name.TabIndex = 39;
-            // 
-            // btn_edit
-            // 
-            this.btn_edit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(82)))), ((int)(((byte)(48)))));
-            this.btn_edit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_edit.ForeColor = System.Drawing.Color.White;
-            this.btn_edit.Location = new System.Drawing.Point(255, 211);
-            this.btn_edit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btn_edit.Name = "btn_edit";
-            this.btn_edit.Size = new System.Drawing.Size(105, 30);
-            this.btn_edit.TabIndex = 29;
-            this.btn_edit.Text = "Edit";
-            this.btn_edit.UseVisualStyleBackColor = false;
-            this.btn_edit.Click += new System.EventHandler(this.btn_save_Click);
-            // 
-            // btn_save
-            // 
-            this.btn_save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(82)))), ((int)(((byte)(48)))));
-            this.btn_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_save.ForeColor = System.Drawing.Color.White;
-            this.btn_save.Location = new System.Drawing.Point(392, 211);
-            this.btn_save.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btn_save.Name = "btn_save";
-            this.btn_save.Size = new System.Drawing.Size(105, 30);
-            this.btn_save.TabIndex = 40;
-            this.btn_save.Text = "Save";
-            this.btn_save.UseVisualStyleBackColor = false;
-            this.btn_save.Click += new System.EventHandler(this.btn_save_Click_1);
-            // 
-            // img_customer_profile
-            // 
-            this.img_customer_profile.Image = global::Tabkha_1._1.Properties.Resources.Max_R_Headshot__1_;
-            this.img_customer_profile.Location = new System.Drawing.Point(327, 12);
-            this.img_customer_profile.Name = "img_customer_profile";
-            this.img_customer_profile.Size = new System.Drawing.Size(100, 100);
-            this.img_customer_profile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.img_customer_profile.TabIndex = 28;
-            this.img_customer_profile.TabStop = false;
-            // 
-            // guna2Elipse6
-            // 
-            this.guna2Elipse9.BorderRadius = 20;
-            this.guna2Elipse9.TargetControl = this.btn_edit_img;
-            // 
-            // btn_save
-            // 
-            this.btn_save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(82)))), ((int)(((byte)(48)))));
-            this.btn_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_save.ForeColor = System.Drawing.Color.White;
-            this.btn_save.Location = new System.Drawing.Point(329, 409);
-            this.btn_save.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btn_save.Name = "btn_save";
-            this.btn_save.Size = new System.Drawing.Size(143, 49);
-            this.btn_save.TabIndex = 32;
-            this.btn_save.Text = "Save";
-            this.btn_save.UseVisualStyleBackColor = false;
-            // 
-            // guna2Elipse7
-            // 
-            this.guna2Elipse7.BorderRadius = 20;
-            this.guna2Elipse7.TargetControl = this.btn_save;
+            this.txtbox_name.Size = new System.Drawing.Size(328, 22);
+            this.txtbox_name.TabIndex = 3;
+            this.txtbox_name.WatermarkColor = System.Drawing.Color.Gray;
+            this.txtbox_name.WatermarkText = "Enter name here";
             // 
             // Manage_Profile
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 428);
-            this.Controls.Add(this.pnl_edit);
+            this.ClientSize = new System.Drawing.Size(661, 297);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_Pnumber);
+            this.Controls.Add(this.lbl_email);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.txtbox_email);
+            this.Controls.Add(this.txtbox_Pnumber);
+            this.Controls.Add(this.txtbox_password);
+            this.Controls.Add(this.txtbox_name);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_edit_img);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Manage_Profile";
-            this.Text = "Manage_Profile";
-            this.Load += new System.EventHandler(this.Manage_Profile_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
-        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
-        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse3;
-        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse4;
-        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse5;
-        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse6;
+        private System.Windows.Forms.Button btn_edit_img;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_edit;
+        private CustomTextBox txtbox_name;
+        private CustomTextBox txtbox_password;
+        private CustomTextBox txtbox_Pnumber;
+        private CustomTextBox txtbox_email;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_email;
+        private System.Windows.Forms.Label lbl_Pnumber;
+        private System.Windows.Forms.Label lbl_password;
     }
 }
--- Tabkha_1.1/Customer/Manage Profile.cs
+++ Tabkha_1.1/Customer/Manage Profile.cs
@@ -17,14 +17,9 @@ namespace Tabkha_1._1
             InitializeComponent();
         }
 
-        private void btn_edit_img_Click(object sender, EventArgs e)
-        {
-
-        }
-
         public void readOnly()
         {
-            txtbox_Bdate.ReadOnly = true;
+            txtbox_password.ReadOnly = true;
             txtbox_email.ReadOnly = true;
             txtbox_name.ReadOnly = true;
             txtbox_Pnumber.ReadOnly = true;
@@ -32,9 +27,9 @@ namespace Tabkha_1._1
             btn_save.Visible = false;
         }
 
-        private void btn_save_Click(object sender, EventArgs e)
+        private void btn_edit_Click(object sender, EventArgs e)
         {
-            txtbox_Bdate.ReadOnly = false;
+            txtbox_password.ReadOnly = false;
             txtbox_email.ReadOnly = false;
             txtbox_name.ReadOnly = false;
             txtbox_Pnumber.ReadOnly = false;
@@ -42,7 +37,7 @@ namespace Tabkha_1._1
             btn_save.Visible = true;
         }
 
-        private void btn_save_Click_1(object sender, EventArgs e)
+        private void btn_save_Click(object sender, EventArgs e)
         {
             readOnly();
         }
