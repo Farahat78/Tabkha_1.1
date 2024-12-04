155efd0684f719e4edb2875a6ae813ad6a6b7b14
asmaa <alnhrawyhamd62@gmail.com>
2024-12-04 21:13:00 +0200

    login

--- Tabkha_1.1/ResetPassword.cs
+++ Tabkha_1.1/ResetPassword.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,7 @@ namespace Tabkha_1._1
         {
             InitializeComponent();
         }
+        string connectionString = @"Data Source=LAPTOP-EBHNP4IJ\;Initial Catalog=tabkha_system;Integrated Security=True";
 
         private void img_close_Click(object sender, EventArgs e)
         {
@@ -33,5 +35,61 @@ namespace Tabkha_1._1
             forgetPassword.Show();
             this.Hide();
         }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            string pass = customTextBox1.Text;
+            string confirm_pass = customTextBox2.Text;
+
+            if (pass != confirm_pass) { MessageBox.Show("Password and confirm password do not match , please try again"); }
+            else
+            {
+                string email = globalvariables.global_email;
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+
+
+                        string query = "update  customers set customer_password=@password, customer_confirmpassword=@confirmpassword where customer_email=@email";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+
+
+                            cmd.Parameters.AddWithValue("@password", pass);
+                            cmd.Parameters.AddWithValue("@confirmpassword", confirm_pass);
+                            cmd.Parameters.AddWithValue("@email", email);
+
+                            int rowaffected = Convert.ToInt32(cmd.ExecuteScalar());
+                            if (rowaffected > 0)
+                            {
+                                user_home user_Home = new user_home();
+                                user_Home.Show();
+                                this.Hide();
+                            }
+                            else
+                            {
+                                login login = new login();
+                                login.Show();
+                                this.Hide();
+
+                            }
+
+
+                        }
+
+
+
+                    }
+                    catch (Exception ex) { MessageBox.Show("errorrrrrr: " + ex.Message); }
+                }
+            }
+
+
+        }
+
+
     }
 }
+
--- Tabkha_1.1/login.cs
+++ Tabkha_1.1/login.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace Tabkha_1._1
 {
@@ -21,6 +24,8 @@ namespace Tabkha_1._1
         {
             Application.Exit();
         }
+        string connectionString = @"Data Source=LAPTOP-EBHNP4IJ\;Initial Catalog=tabkha_system;Integrated Security=True";
+        private string query;
 
         private void img_minimize_Click(object sender, EventArgs e)
         {
@@ -34,15 +39,85 @@ namespace Tabkha_1._1
 
         private void btn_signIn_Click(object sender, EventArgs e)
         {
-            if (txt_password.Text == ""
+
+            string user_name = txt_username.Text;
+            string password = txt_password.Text;
+            if (chk_rememberMe.Checked == true)
+            {
+                if (user_name == "")
+                {
+                    MessageBox.Show("please enter you email");
+                }
+                else
+                {
+                    Sendcode(user_name);
+                    MessageBox.Show("we send password to you if your email is correct");
+                }
+            }
+            
+
+           if (txt_password.Text == ""
                 || txt_username.Text == "")
             { MessageBox.Show("Required Fields Empty"); }
+
             else
             {
-                if(txt_username.Text == "" && txt_password.Text == "")
+                using (SqlConnection conn = new SqlConnection(connectionString))
                 {
+                    try
+                    {
+                        conn.Open();
+                        if (user_name.Contains('@') == true)
+                        {
+                            query = "select count(1) from customers where customer_email=@email and customer_password=@password";
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@email", user_name);
+                                cmd.Parameters.AddWithValue("@password", password);
+                                int rowaffected = Convert.ToInt32(cmd.ExecuteScalar());
+                                if (rowaffected > 0)
+                                {
+                                    user_home user_Home = new user_home();
+                                    user_Home.Show();
+                                    this.Hide();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            query = "select count(1) from customers where customer_phonen=@phone and customer_password=@password";
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@phone", user_name);
+                                cmd.Parameters.AddWithValue("@password", password);
+                                int rowaffected = Convert.ToInt32(cmd.ExecuteScalar());
+                                if (rowaffected > 0)
+                                {
+                                    user_home user_Home = new user_home();
+                                    user_Home.Show();
+                                    this.Hide();
+                                }
+                                else
+                                {
+                                   
+                                    
+                                        MessageBox.Show("login failed , try again");
+                                    
+                                }
+                            }
 
+                        }
+
+
+                    }
+
+
+
+
+                    catch (Exception ex) { MessageBox.Show("errorrrrrr: " + ex.Message); }
                 }
+
+
             }
         }
 
@@ -50,6 +125,69 @@ namespace Tabkha_1._1
         {
             option_for_register option = new option_for_register();
             option.Show();
+        }
+
+        private void L_lbl_forgetPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            forget forget = new forget();
+            forget.Show();
+            this.Hide();
+        }
+        private void Sendcode(string user_email)
+        {
+            string password = "";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                
+                
+                    conn.Open();
+                   
+                        query = "select customer_password from customers where customer_email=@email ";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+
+                             cmd.Parameters.AddWithValue("@email", user_email);
+                             using (SqlDataReader reader = cmd.ExecuteReader())
+                             {
+                                   if(reader.Read())
+                                   {
+                                        password = reader["customer_password"].ToString();
+                                        
+
+                                   }
+
+                             }
+
+                   
+                        }   
+                    
+
+                   
+
+
+                
+            }
+
+
+            string subject = "Welcome to Our System!";
+            string body = $"  your password is   {password}    ...!";
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress("asma37amd@gmail.com");
+                mail.To.Add(user_email);
+                mail.Subject = subject;
+                mail.Body = body;
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com"))
+                {
+                    smtp.Port = 587;
+                    smtp.Credentials = new System.Net.NetworkCredential("asma37amd@gmail.com", "hugveqskeeqrabuk");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+
         }
     }
 }
