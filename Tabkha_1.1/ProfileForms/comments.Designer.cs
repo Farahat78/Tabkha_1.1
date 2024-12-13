e1fc12418f2cbe5abaa1d23544498847341f3397
Hossam Mostafa <shicoo9911@gmail.com>
2024-12-13 04:39:51 +0200

    df

--- Tabkha_1.1/ProfileForms/comments.Designer.cs
+++ Tabkha_1.1/ProfileForms/comments.Designer.cs
@@ -33,17 +33,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_nme = new System.Windows.Forms.Label();
+            this.lbl_comments = new System.Windows.Forms.Label();
+            this.panelTemplate = new System.Windows.Forms.Panel();
+            this.stars = new Guna.UI2.WinForms.Guna2RatingStar();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.img_profile)).BeginInit();
+            this.panelTemplate.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // img_profile
             // 
             this.img_profile.Image = global::Tabkha_1._1.Properties.Resources.Max_R_Headshot__1_;
-            this.img_profile.Location = new System.Drawing.Point(8, 60);
+            this.img_profile.Location = new System.Drawing.Point(3, 3);
             this.img_profile.Name = "img_profile";
-            this.img_profile.Size = new System.Drawing.Size(80, 80);
+            this.img_profile.Size = new System.Drawing.Size(71, 67);
             this.img_profile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.img_profile.TabIndex = 0;
             this.img_profile.TabStop = false;
@@ -52,9 +57,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(12, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 34);
+            this.label1.Size = new System.Drawing.Size(154, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Comments";
             // 
@@ -62,9 +67,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Gray;
-            this.label2.Location = new System.Drawing.Point(0, 34);
+            this.label2.Location = new System.Drawing.Point(0, 29);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(225, 19);
+            this.label2.Size = new System.Drawing.Size(175, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "________________________";
             // 
@@ -73,41 +78,70 @@
             this.guna2Elipse1.BorderRadius = 100;
             this.guna2Elipse1.TargetControl = this.img_profile;
             // 
-            // label3
+            // lbl_nme
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(98, 69);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(161, 24);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Ahmed Mohamed";
+            this.lbl_nme.AutoSize = true;
+            this.lbl_nme.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nme.Location = new System.Drawing.Point(83, 10);
+            this.lbl_nme.Name = "lbl_nme";
+            this.lbl_nme.Size = new System.Drawing.Size(138, 21);
+            this.lbl_nme.TabIndex = 3;
+            this.lbl_nme.Text = "Ahmed Mohamed";
+            this.lbl_nme.Click += new System.EventHandler(this.label3_Click);
             // 
-            // label4
+            // lbl_comments
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(102, 97);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(287, 22);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Greet Restaurant wish you all the best";
+            this.lbl_comments.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_comments.Location = new System.Drawing.Point(87, 34);
+            this.lbl_comments.Name = "lbl_comments";
+            this.lbl_comments.Size = new System.Drawing.Size(449, 63);
+            this.lbl_comments.TabIndex = 4;
+            this.lbl_comments.Text = "Greet Restaurant wish you all the best";
+            // 
+            // panelTemplate
+            // 
+            this.panelTemplate.Controls.Add(this.stars);
+            this.panelTemplate.Controls.Add(this.img_profile);
+            this.panelTemplate.Controls.Add(this.lbl_comments);
+            this.panelTemplate.Controls.Add(this.lbl_nme);
+            this.panelTemplate.Location = new System.Drawing.Point(3, 3);
+            this.panelTemplate.Name = "panelTemplate";
+            this.panelTemplate.Size = new System.Drawing.Size(708, 105);
+            this.panelTemplate.TabIndex = 5;
+            // 
+            // stars
+            // 
+            this.stars.Location = new System.Drawing.Point(542, 24);
+            this.stars.Name = "stars";
+            this.stars.Size = new System.Drawing.Size(120, 28);
+            this.stars.TabIndex = 5;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.panelTemplate);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 48);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(711, 331);
+            this.flowLayoutPanel1.TabIndex = 6;
             // 
             // comments
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(711, 379);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.img_profile);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "comments";
             this.Text = "comments";
             ((System.ComponentModel.ISupportInitialize)(this.img_profile)).EndInit();
+            this.panelTemplate.ResumeLayout(false);
+            this.panelTemplate.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,7 +153,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_nme;
+        private System.Windows.Forms.Label lbl_comments;
+        private System.Windows.Forms.Panel panelTemplate;
+        private Guna.UI2.WinForms.Guna2RatingStar stars;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- Tabkha_1.1/ProfileForms/comments.cs
+++ Tabkha_1.1/ProfileForms/comments.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tabkha_1._1.Class;
 
 namespace Tabkha_1._1
 {
@@ -15,6 +17,125 @@ namespace Tabkha_1._1
         public comments()
         {
             InitializeComponent();
+            this.Dock = DockStyle.Fill;
+        }
+
+        private Control CloneControl(Control control)
+        {
+            Control newControl = (Control)Activator.CreateInstance(control.GetType());
+            newControl.Text = control.Text;
+            newControl.Size = control.Size;
+            newControl.Location = control.Location;
+            newControl.Font = control.Font;
+            newControl.BackColor = control.BackColor;
+            newControl.ForeColor = control.ForeColor;
+            newControl.Name = control.Name;
+
+            // Copy other properties as needed
+            return newControl;
+        }
+
+        private void LoadReviews()
+        {
+            try
+            {
+                string query = @"
+                    SELECT 
+                        Reviews.ReviewID, 
+                        Reviews.Comment, 
+                        Reviews.Rating, 
+                        Users.Fname, 
+                        Users.Lname, 
+                        Users.ProfilePic 
+                    FROM 
+                        Reviews 
+                    INNER JOIN 
+                        Users 
+                    ON 
+                        Reviews.UserID = Users.UserID 
+                    WHERE 
+                        Reviews.ChefID = @ChefID"; // Use the ChefID of the current chef
+
+                using (SqlConnection connection = new SqlConnection(Connection.connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@ChefID", Session.Id); // Get the ChefID from the session
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    //flowLayoutPanel1.Controls.Clear(); // Clear existing comments
+
+                    while (reader.Read())
+                    {
+                        // Clone the existing comment card template
+                        Panel newCard = CloneCommentCard();
+
+                        // Update profile picture
+                        PictureBox profilePicBox = newCard.Controls.OfType<PictureBox>().FirstOrDefault(c => c.Name == "img_profile");
+                        string profilePicPath = reader["ProfilePic"]?.ToString();
+                        if (profilePicBox != null)
+                        {
+                            profilePicBox.Image = !string.IsNullOrEmpty(profilePicPath)
+                                ? Image.FromFile(profilePicPath)
+                                : Properties.Resources.Max_R_Headshot__1_; // Default image if none provided
+                            profilePicBox.SizeMode = PictureBoxSizeMode.Zoom;
+                        }
+
+                        // Update name
+                        Label nameLabel = newCard.Controls.OfType<Label>().FirstOrDefault(c => c.Name == "lbl_nme");
+                        if (nameLabel != null)
+                        {
+                            nameLabel.Text = $"{reader["Fname"]} {reader["Lname"]}";
+                        }
+
+                        // Update rating (e.g., stars)
+                        Label ratingLabel = newCard.Controls.OfType<Label>().FirstOrDefault(c => c.Name == "stars");
+                        if (ratingLabel != null)
+                        {
+                            ratingLabel.Text = new string('★', (int)reader["Rating"]); // Display stars based on rating
+                        }
+
+                        // Update comment text
+                        Label commentLabel = newCard.Controls.OfType<Label>().FirstOrDefault(c => c.Name == "lbl_comments");
+                        if (commentLabel != null)
+                        {
+                            commentLabel.Text = reader["Comment"].ToString();
+                        }
+
+                        // Add the comment card to the FlowLayoutPanel
+                        flowLayoutPanel1.Controls.Add(newCard);
+                    }
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading reviews: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Panel CloneCommentCard()
+        {
+            Panel newCard = new Panel
+            {
+                Size = panelTemplate.Size,
+                BackColor = panelTemplate.BackColor,
+                BorderStyle = panelTemplate.BorderStyle
+            };
+
+            foreach (Control control in panelTemplate.Controls)
+            {
+                Control clonedControl = CloneControl(control);
+                newCard.Controls.Add(clonedControl);
+            }
+
+            return newCard;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
